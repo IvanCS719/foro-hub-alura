80a354b5b1b3e44778e0dcc9bfe2e79aa4eb7dff
IvanCS719 <ivancupil17@gmail.com>
2024-07-09 14:53:10 -0600

    Inicializando proyecto

---
+++ src/main/java/com/ivandroid/foro_hub_alura/ForoHubAluraApiApp.java
@@ -0,0 +1,13 @@
+package com.ivandroid.foro_hub_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubAluraApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubAluraApiApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivandroid/foro_hub_alura/ForoHubAluraApiAppTests.java
@@ -0,0 +1,13 @@
+package com.ivandroid.foro_hub_alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubAluraApiAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
