4255ffa2fec0f6ea0e5e8c1fd2adfea25faca0fb
IvanCS719 <ivancupil17@gmail.com>
2024-07-13 11:39:09 -0600

    Creando el servicio de autenticacion e implemantar la interfaz UserDetailsService para perzanolizar el flujo de autenticacion por defecto

--- src/main/java/com/ivandroid/foro_hub_alura/domain/user/UsuarioRepository.java
+++ src/main/java/com/ivandroid/foro_hub_alura/domain/user/UsuarioRepository.java
@@ -1,6 +1,8 @@
 package com.ivandroid.foro_hub_alura.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    UserDetails findByCorreoElectronico(String correoElectronico);
 }
