97d4a055cae1a8a34a379599c9aef584d90310c3
IvanCS719 <ivancupil17@gmail.com>
2024-07-15 01:07:54 -0600

    Liberando el acceso al login y probando el control de acceso para las otras urls

--- src/main/java/com/ivandroid/foro_hub_alura/infrastructure/security/SecurityFilter.java
+++ src/main/java/com/ivandroid/foro_hub_alura/infrastructure/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package com.ivandroid.foro_hub_alura.infrastructure.security;
 
+import com.ivandroid.foro_hub_alura.domain.user.UsuarioRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,16 +19,25 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        var token = request.getHeader("Authorization");
-        if (token == null || token.isEmpty()){
-            throw new RuntimeException("Token no válido");
+        //Obtner token del header
+        var authToken = request.getHeader("Authorization");
+        if (authToken != null){
+            var token = authToken.replace("Bearer ", "");
+            var subject = tokenService.getSubject(token);
+            if(subject != null){
+                //Token valido
+                var usuario = usuarioRepository.findByCorreoElectronico(subject);
+                var authetication = new UsernamePasswordAuthenticationToken(usuario,
+                        null, usuario.getAuthorities()); //Forzar inicio de sesión
+                SecurityContextHolder.getContext().setAuthentication(authetication);
+            }
         }
-        token = token.replace("Bearer ", "");
-        System.out.println(token);
-        System.out.println(tokenService.getSubject(token));
-        filterChain.doFilter(request, response);
+        filterChain.doFilter(request,response);
     }
 }
